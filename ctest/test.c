467dee8cb795c874d77e203eba9e8bc7ffc21ebc
Vlad-Stefan Harbuz <vlad@vladh.net>
2021-03-06 04:25:41 +0100

    Realise variables exist

---
+++ ctest/test.c
@@ -0,0 +1,11 @@
+int var1 = 5;
+int var2;
+
+int main() {
+  var1 = 3;
+  var2 = 10;
+  int var3 = var1 + var2;
+  int var4 = 3;
+  int var5;
+  return 0;
+}
--- src/hello.asm
+++ src/hello.asm
@@ -9,7 +9,7 @@ board_height dq 15
 board_width dq 30
 newline db 0xd, 0xa, 0
 board_icon_empty db ".", 0
-board_icon_test db "!", 0
+board_icon_food db "*", 0
 board_icon_head db "O", 0
 fmt_number db "%d", 0xd, 0xa, 0
 fmt_string db "%s", 0xd, 0xa, 0
@@ -33,6 +33,11 @@ clear_board:
   push rbp
   mov rbp, rsp
   sub rsp, 32
+
+  mov rdx, 0
+  mov r8, 0
+  mov rcx, seq_pos
+  call printf
 
   mov rcx, seq_clear
   call printf
@@ -70,11 +75,7 @@ print_board:
 .r13_storage equ 40
   push rbp
   mov rbp, rsp
-  sub rsp, 32
-
-  mov rcx, fmt_number
-  mov rdx, [rbp + 16]
-  call printf
+  sub rsp, 64
 
   mov [rsp + .r12_storage], r12
   mov [rsp + .r13_storage], r13
@@ -111,17 +112,28 @@ print_board:
 
 
 main:
+.r12_storage equ 32
+.r13_storage equ 40
+.r14_storage equ 48
+.r15_storage equ 56
   push rbp
   mov rbp, rsp
-  sub rsp, 32
+  sub rsp, 64
+
+  mov [rsp + .r12_storage], r12
+  mov [rsp + .r13_storage], r13
+  mov [rsp + .r14_storage], r14
+  mov [rsp + .r15_storage], r15
 
   call _CRT_INIT
 
   call clear_board
 
+  call print_board
+
   mov rcx, 10
   mov rdx, 10
-  mov r8, board_icon_test
+  mov r8, board_icon_food
   call print_char
 
   mov rcx, 2
@@ -129,11 +141,11 @@ main:
   mov r8, board_icon_head
   call print_char
 
-  ; push 5
-  ; call print_board
-  ; add rsp, 8 ; pop
-
-  xor rax, rax
+  mov r12, [rsp + .r12_storage]
+  mov r13, [rsp + .r13_storage]
+  mov r14, [rsp + .r14_storage]
+  mov r15, [rsp + .r15_storage]
   mov rsp, rbp
   pop rbp
+  xor rax, rax
   call ExitProcess
