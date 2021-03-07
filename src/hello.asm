c2417a450f52c4e3866677f4b496d5a7fa3507a6
Vlad-Stefan Harbuz <vlad@vladh.net>
2021-03-07 14:25:19 +0100

    Add moving around

--- src/hello.asm
+++ src/hello.asm
@@ -11,7 +11,8 @@ NEWLINE db 0xd, 0xa, 0
 BOARD_ICON_EMPTY db ".", 0
 BOARD_ICON_FOOD db "*", 0
 BOARD_ICON_HEAD db "O", 0
-FMT_NUMBER db "%d", 0xd, 0xa, 0
+FMT_INT db "%d", 0xd, 0xa, 0
+FMT_UINT db "%u", 0xd, 0xa, 0
 FMT_CHAR db "%c", 0xd, 0xa, 0
 FMT_STRING db "%s", 0xd, 0xa, 0
 SEQ_CLEAR db 0x1b, 0x5b, "2J", 0
@@ -19,6 +20,11 @@ SEQ_POS db 0x1b, 0x5b, "%d;%dH", 0
 SEQ_BLUE db 0x1b, 0x5b, "34m", 0
 SEQ_RESET db 0x1b, 0x5b, "0m", 0
 STD_INPUT_HANDLE dq -10
+INPUT_UP db "w"
+INPUT_DOWN db "s"
+INPUT_LEFT db "a"
+INPUT_RIGHT db "d"
+INPUT_QUIT db "q"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -45,6 +51,12 @@ extern ReadFile
 ; );
 extern ExitProcess
 
+; BOOL WINAPI SetConsoleMode(
+;   _In_ HANDLE hConsoleHandle,
+;   _In_ DWORD  dwMode
+; );
+extern SetConsoleMode
+
 extern _CRT_INIT
 extern printf
 
@@ -52,24 +64,33 @@ extern printf
 read_char:
 .char equ 16
 .nBytesRead equ 24
+.handle equ 32
+.scratch equ 40
   push rbp
   mov rbp, rsp
-  sub rsp, 64
+  sub rsp, 32
 
-  mov rcx, [STD_INPUT_HANDLE]
+  ; Get the standard input handle
+  mov rcx, [STD_INPUT_HANDLE] ; nStdHandle
   call GetStdHandle
+  mov [rbp + .handle], rax
 
-  mov rcx, rax ; hFile
+  ; Disable all input modes
+  ; This means the typed character won't be printed, and we also will not
+  ; wait for an <Enter> after it.
+  mov rcx, rax ; hConsoleHandle
+  mov rdx, 0 ; dwMode
+  call SetConsoleMode
+
+  ; Read the character
+  mov rcx, [rbp + .handle] ; hFile
   lea rdx, [rbp + .char] ; lpBuffer
   mov r8, 1 ; nNumberOfBytesToRead
   lea r9, [rbp + .nBytesRead] ; lpNumberOfBytesRead
-  mov qword [rsp + 32], 0 ; lpOverlapped
+  mov qword [rbp + .scratch], 0 ; lpOverlapped
   call ReadFile
 
-  mov rcx, FMT_CHAR
-  mov rdx, [rbp + 16]
-  call printf
-
+  mov rax, [rbp + .char]
   mov rsp, rbp
   pop rbp
   ret
@@ -102,8 +123,7 @@ print_char: ; (x, y, char)
 
   mov [rbp + .char], r8
 
-  mov rdx, rcx
-  mov r8, rdx
+  mov r8, rcx
   mov rcx, SEQ_POS
   call printf
 
@@ -166,33 +186,79 @@ main:
   mov rbp, rsp
   sub rsp, 64
 
-  mov byte [rsp + .head_x], 3
+  call _CRT_INIT
+
+  ; Init positions
+  mov byte [rsp + .head_x], 10
   mov byte [rsp + .head_y], 3
   mov byte [rsp + .food_x], 10
-  mov byte [rsp + .food_x], 10
+  mov byte [rsp + .food_x], 5
 
-  call _CRT_INIT
+  .loop:
+    ; Clear and print board
+    call clear_board
+    call print_board
+
+    ; Print food
+    mov rcx, [rsp + .food_x] ; x
+    mov rdx, [rsp + .food_y] ; y
+    mov r8, BOARD_ICON_FOOD ; char
+    call print_char
+
+    ; Print head
+    mov rcx, [rsp + .head_x] ; x
+    mov rdx, [rsp + .head_y] ; y
+    mov r8, BOARD_ICON_HEAD ; char
+    call print_char
+
+    ; Reset position to 0, 0
+    mov rdx, 0
+    mov r8, 0
+    mov rcx, SEQ_POS
+    call printf
+
+    ; Read character
+    call read_char
+
+    ; Do something with character
+    cmp al, [INPUT_UP]
+    je .action_up
+    cmp al, [INPUT_DOWN]
+    je .action_down
+    cmp al, [INPUT_LEFT]
+    je .action_left
+    cmp al, [INPUT_RIGHT]
+    je .action_right
+    cmp al, [INPUT_QUIT]
+    je .action_quit
+    jmp .end_input
+
+    .action_up:
+    dec byte [rsp + .head_y]
+    jmp .end_input
+
+    .action_down:
+    inc byte [rsp + .head_y]
+    jmp .end_input
+
+    .action_left:
+    dec byte [rsp + .head_x]
+    jmp .end_input
+
+    .action_right:
+    inc byte [rsp + .head_x]
+    jmp .end_input
+
+    .action_quit:
+    jmp .end_loop
+
+    .end_input:
+    jmp .loop
+  .end_loop:
+
   call clear_board
-  call print_board
 
-  mov rcx, [rsp + .food_x] ; x
-  mov rdx, [rsp + .food_y] ; y
-  mov r8, BOARD_ICON_FOOD ; char
-  call print_char
-
-  mov rcx, [rsp + .head_x] ; x
-  mov rdx, [rsp + .head_y] ; y
-  mov r8, BOARD_ICON_HEAD ; char
-  call print_char
-
-  mov rdx, 0
-  mov r8, 0
-  mov rcx, SEQ_POS
-  call printf
-
-  call read_char
-
+  xor rax, rax
   mov rsp, rbp
   pop rbp
-  xor rax, rax
   call ExitProcess
